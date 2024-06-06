ac7c3269e950aec67394cafbf5c600a47d0eae31
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2024-06-07 02:22:33 +0530

    Add logs

--- index.js
+++ index.js
@@ -9,37 +9,44 @@ const PORT = process.env.PORT || 5000;
 app.set("trust proxy", true);
 
 app.get("/ip-lookup", async (req, res) => {
-    // Get the IP address of the client
-    let ipAddress = req.ip;
+    let clientIpAddress =
+        req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+
+    console.log("clientIpAddress: ", clientIpAddress);
     let ipInfoData = {};
+    let ipv4 = "";
+    let ipv6 = "";
 
     try {
+        // Fetch IP information from ipinfo.io
         const ipInfoResponse = await axios.get(
-            `https://ipinfo.io/${ipAddress}?token=${process.env.IPINFO_TOKEN}`
+            `https://ipinfo.io/${clientIpAddress}?token=${process.env.IPINFO_TOKEN}`
         );
         ipInfoData = ipInfoResponse.data;
     } catch (error) {
         console.error("Error fetching data from ipinfo:", error);
     }
 
-    let ipv4 = "";
-    let ipv6 = "";
-
     try {
-        const ipv4Response = await axios.get(`https://api.ipify.org`);
+        // Fetch IPv4 address
+        const ipv4Response = await axios.get("https://api.ipify.org");
+        console.log("ipv4Response: ", ipv4Response);
         ipv4 = ipv4Response.data;
     } catch (error) {
         console.error("Error fetching IPv4 address:", error);
     }
 
     try {
-        const ipv6Response = await axios.get(`https://api6.ipify.org`);
+        // Fetch IPv6 address
+        const ipv6Response = await axios.get("https://api6.ipify.org");
+        console.log("ipv6Response: ", ipv6Response);
         ipv6 = ipv6Response.data;
     } catch (error) {
         console.error("Error fetching IPv6 address:", error);
     }
 
     res.json({
+        clientIpAddress: clientIpAddress,
         ipv4: ipv4,
         ipv6: ipv6,
         ipInfo: ipInfoData,
