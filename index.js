dfc0f5caab08b7ec53423561eeac46ec7544edf3
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2024-06-09 00:32:08 +0530

    Update fallback reponse

--- index.js
+++ index.js
@@ -3,118 +3,66 @@ const axios = require("axios");
 const geoip = require("geoip-lite");
 const app = express();
 const PORT = process.env.PORT || 5000;
+let Country = require("country-state-city").Country;
+let State = require("country-state-city").State;
 
 app.set("trust proxy", true);
 
-app.get("/ip-lookup", async (req, res) => {
-    let clientIp = req.ip;
-    let xForwardedFor = req.headers["x-forwarded-for"];
-    let ipv4 = "";
-    let ipv6 = "";
-    let ipList = [];
-
-    if (xForwardedFor) {
-        ipList = xForwardedFor.split(",");
-        ipList.forEach((ip) => {
-            ip = ip.trim();
-            if (ip.includes(":")) {
-                ipv6 = ip;
-            }
-        });
-    }
-
-    if (clientIp.includes(":")) {
-        ipv6 = clientIp;
-    } else {
-        ipv4 = clientIp;
-    }
-
-    let geoData = geoip.lookup(clientIp);
-    let ipInfoData = {};
-    let response = {
-        ipv4: ipv4 || "",
-        ipv6: ipv6 || "",
-    };
+app.get("/ip/:ipAddress", async (req, res) => {
+    const { ipAddress } = req.params;
+    let response = {};
 
     try {
-        let res = await axios.get(
-            `http://ip-api.com/json/${ipv4 ? ipv4 : ipv6}`
-        );
-        ipInfoData = res.data;
-    } catch (error) {
-        console.log("error in ip-info : ", error);
-    }
+        let apiResponse = await axios.post("https://api2.ip8.com/ip/info", {
+            ip: [ipAddress],
+        });
+        let ip8Data = apiResponse.data.data[ipAddress];
 
-    if (ipInfoData && ipInfoData?.status === "success") {
-        response = {
-            ...response,
-            country: ipInfoData.country || "",
-            countryCode: ipInfoData.countryCode || "",
-            region: ipInfoData.region || "",
-            regionName: ipInfoData.regionName || "",
-            city: ipInfoData.city || "",
-            zip: ipInfoData.zip || "",
-            latitude: ipInfoData.lat || "",
-            longitude: ipInfoData.lon || "",
-            timezone: ipInfoData.timezone || "",
-            isp: ipInfoData.isp || "",
-            org: ipInfoData.org || "",
-        };
-    } else if (geoData) {
-        response = {
-            ...response,
-            city: geoData.city || "",
-            region: geoData.region || "",
-            country: geoData.country || "",
-            latitude: geoData.ll ? geoData.ll[0] : "",
-            longitude: geoData.ll ? geoData.ll[1] : "",
-            timezone: geoData.timezone || "",
-        };
-    } else {
-        response.geo = { error: "Location not found" };
+        // Optional cleanup
+        delete ip8Data.headers;
+        delete ip8Data.useragent;
+        delete ip8Data.isTorIp;
+        delete ip8Data.ct;
+
+        response = { ...ip8Data };
+    } catch (error) {
+        console.log("Failed for ip2.ip8:", error);
+
+        // Use geoip-lite as fallback
+        let geoData = geoip.lookup(ipAddress);
+        if (geoData) {
+            response = {
+                geoip: {
+                    city: geoData.city || null,
+                    country:
+                        Country.getCountryByCode(geoData?.country).name || null,
+                    continent: geoData.timezone.split("/")[0] || null,
+                    latitude: geoData.ll ? geoData.ll[0] : null,
+                    longitude: geoData.ll ? geoData.ll[1] : null,
+                    postalcode: geoData.postal || null,
+                    region:
+                        State.getStateByCodeAndCountry(
+                            geoData?.region,
+                            geoData?.country
+                        ).name || null,
+                    isocode: geoData.country || null,
+                    timezone: geoData.timezone || null,
+                },
+                isp: {
+                    autonomousSystemNumber: null,
+                    autonomousSystemOrganization: null,
+                    ipAddress: ipAddress,
+                    network: null,
+                    isp: null,
+                    organization: null,
+                },
+            };
+        } else {
+            response.geoip = { error: "Location not found" };
+        }
     }
 
     res.json(response);
-});
-
-app.get("/ip8", async (req, res) => {
-    try {
-        const response = await axios.post("https://api2.ip8.com/ip/info", {
-            clientIP: "43.249.228.84",
-        });
-        res.json(response.data);
-    } catch (error) {
-        console.error("Error:", error);
-        res.status(500).send("Internal Server Error");
-    }
-});
-
-app.get("/ip4", async (req, res) => {
-    try {
-        const clientIP = req.ip;
-        console.log("ipv4 clientIP: ", clientIP);
-        const response = await axios.post("https://ip4.ip8.com/", {
-            clientIP,
-        });
-        res.json(response.data);
-    } catch (error) {
-        console.error("Error:", error);
-        res.status(500).send("Internal Server Error");
-    }
-});
-
-app.get("/ip6", async (req, res) => {
-    try {
-        const clientIP = req.ip;
-        console.log("ipv6 clientIP: ", clientIP);
-        const response = await axios.post("https://ip6.ip8.com/", {
-            clientIP,
-        });
-        res.json(response.data);
-    } catch (error) {
-        console.error("Error:", error);
-        res.status(500).send("Internal Server Error");
-    }
 });
 
 app.listen(PORT, () => {
